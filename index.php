e046288922ce269b63f5a8c7ef1b742f44d382d7
edwardtising <34829119+edwardtising@users.noreply.github.com>
2018-01-09 18:18:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
-    $animals = [
+
+$animals = [
     'America' => ['Mammuthus columbi', 'Abydosaurus', 'Coccyzus', 'Neovison vison', 'Burhinidae'],
-    'Africa' => ['Acinonyx', 'Vulpes pallida', 'Etmopterus polli', 'Papio', 'Connochaetes taurinus'),
-    'Asia' => ['Harpactes erythrocephalus', 'Aplocheilus', 'Bubalus arnee', 'Adasaurus', 'Elephas maximus')
+    'Africa' => ['Acinonyx', 'Vulpes pallida', 'Etmopterus polli', 'Papio', 'Connochaetes taurinus'],
+    'Asia' => ['Harpactes erythrocephalus', 'Aplocheilus', 'Bubalus arnee', 'Adasaurus', 'Elephas maximus']
 ];
-    $result = [];
+echo '<pre>';
+print_r($animals);
+
+$result = [];
     foreach ($animals as $countinent) {
         $result = array_merge($result, array_filter($countinent, function ($item){ return count(explode(' ', $item)) === 2; }) );
 }
-    print_r($result);
-     ?>
+echo '<pre>';
+print_r($result);
+?>
