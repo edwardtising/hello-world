b3d9c95df4ce079f3ccc667ac92645abe722d66e
edwardtising <34829119+edwardtising@users.noreply.github.com>
2018-06-29 22:53:38 +0300

    Update lesson_1.php

--- lesson_1.php
+++ lesson_1.php
@@ -1,6 +1,7 @@
 <?php
 $username = 'Сергей';
 $userage = 35;
+$city = 'Сочи';
 $usermail = 'sdevelov@mail.ru';
 $aboutuser = 'Back-end Web Development';
 if ($username) {
@@ -8,6 +9,7 @@ if ($username) {
 <table>
 <tr> <td>Имя</td> <td><?= $username ?></td></tr>
 <tr> <td>Возраст</td> <td><?= $userage ?></td></tr>
+<tr> <td>Город</td> <td><?= $city ?></td></tr>  
 <tr> <td>Адрес электронной почты</td> <td><a href="#" color=blue><ins datetime="2018-07-01"><?= $usermail ?></ins></a></td></tr>
 <tr> <td>О себе</td> <td><?= $aboutuser ?></td></tr>
 </table>
